da3dbda81c7928565c59d49ce070f21d410c4734
hexxend <hexxend@sdf.org>
2017-11-25 10:21:43 -0500

    update proxy_scrape

--- proxy_scrape.py
+++ proxy_scrape.py
@@ -12,39 +12,55 @@ from bs4 import BeautifulSoup
 from sys import argv
 import os
 
-url = "https://www.premproxy.com/socks-list/"
+urls = ["https://www.premproxy.com/socks-list/", 'http://www.idcloak.com/proxylist/socks-proxy-list.html']
 
-def get_parsed_page(url):
+def get_parsed_page():
     user_dir = os.environ['HOME']
     proxy_list = '%s/proxy_list' % user_dir
-    sess = requests.Session()
-    req = sess.get(url)
-    content = req.content
-
-    soup = BeautifulSoup(content, 'lxml')
-    ip_data = []
-    type_data = []
-    table = soup.find('table')
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    print("Gathering proxy list")
-
     if proxy_list == True:
         os.remove(proxy_list)
+    sess = requests.Session()
+    for url in urls:
+        req = sess.get(url)
+        content = req.content
 
-    for row in rows:
-        cols = row.find_all('td')
-        #cols = [x.text.strip() for x in cols]
-        rn = str(cols[0].text.strip())
-        rn1 = str(cols[1].text.strip())
-        rn = re.sub(':', ' ', rn)
-        rn1 = rn1.lower()
-        ip_data.append(rn)
-        type_data.append(rn1)
+        soup = BeautifulSoup(content, 'lxml')
+        if url == urls[0]:
+            ip_data = []
+            type_data = []
+            table = soup.find('table')
+            table_body = table.find('tbody')
+            rows = table_body.find_all('tr')
 
-        with open(proxy_list, 'a') as proxy_file:
-            proxy_file.write('%s %s\n' % (rn1, rn))
+            for row in rows:
+                cols = row.find_all('td')
+                #cols = [x.text.strip() for x in cols]
+                rn = str(cols[0].text.strip())
+                rn1 = str(cols[1].text.strip())
+                rn = re.sub(':', ' ', rn)
+                rn1 = rn1.lower()
+                ip_data.append(rn)
+                type_data.append(rn1)
 
-get_parsed_page(url)
+                with open(proxy_list, 'a') as proxy_file:
+                    proxy_file.write('%s %s\n' % (rn1, rn))
+
+        if url == urls[1]:
+            port_data = []
+            table = soup.find('table', id='sort')
+            rows = table.find_all('tr')
+
+            for row in rows:                     # Why are xols out of range?
+                cols = row.find_all('td')
+                rn = str(cols[].text.strip())
+                rn1 = str(cols[].text.strip())
+                rn2 = str(cols[].text.strip())
+                type_data.append(rn)
+                port_data.append(rn1)
+                ip_data.append(r2)
+                proxy_file.write('%s %s %s\n' % (rn, rn2, rn1))
+
+    print("Gathering proxy list")
+get_parsed_page()
 print('list writen')
 exit()
