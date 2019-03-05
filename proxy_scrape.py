aa3060aeb5ad8b69eefaf8a9a13cf8a299e552fc
hexxend <hexxend@protonmail.com>
2019-03-05 08:35:51 -0500

    Fix broken parsing/add license

--- proxy_scrape.py
+++ proxy_scrape.py
@@ -2,7 +2,7 @@
 """
 scrape ips and proxy type from proxy list sites
 and append them to the proxychains config file
-2017 HexXend
+2018 HexXend
 
 TODO;
 scrape more sites
@@ -12,55 +12,76 @@ from bs4 import BeautifulSoup
 from sys import argv
 import os
 
-urls = ["https://www.premproxy.com/socks-list/", 'http://www.idcloak.com/proxylist/socks-proxy-list.html']
+user_dir = os.environ['HOME']                # Use the HOME envrionment variable
+list_dir = '%s/proxy_list' % user_dir
+proxy_list = '%s/proxy_list/proxy_list.file' % user_dir # set where to save the list
 
-def get_parsed_page():
-    user_dir = os.environ['HOME']
-    proxy_list = '%s/proxy_list' % user_dir
-    if proxy_list == True:
-        os.remove(proxy_list)
+def get_parsed_page_one():
+    """
+        Makes a request to the specified URL, creates a session and gets
+        the page contents. 
+
+        The contents are parsed for the table elements and the text is 
+        scraped from the table elements.
+    """
+    url = "https://www.socks-proxy.net"
+
     sess = requests.Session()
-    for url in urls:
-        req = sess.get(url)
-        content = req.content
+    req = sess.get(url)
+    content = req.content
 
-        soup = BeautifulSoup(content, 'lxml')
-        if url == urls[0]:
-            ip_data = []
-            type_data = []
-            table = soup.find('table')
-            table_body = table.find('tbody')
-            rows = table_body.find_all('tr')
+    soup = BeautifulSoup(content, 'lxml')
+    table = soup.find('table')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
 
-            for row in rows:
-                cols = row.find_all('td')
-                #cols = [x.text.strip() for x in cols]
-                rn = str(cols[0].text.strip())
-                rn1 = str(cols[1].text.strip())
-                rn = re.sub(':', ' ', rn)
-                rn1 = rn1.lower()
-                ip_data.append(rn)
-                type_data.append(rn1)
 
-                with open(proxy_list, 'a') as proxy_file:
-                    proxy_file.write('%s %s\n' % (rn1, rn))
+    for row in rows:
+        cols = row.find_all('td')
+        try:
+            rn = str(cols[0].text.strip())
+            rn1 = str(cols[1].text.strip())
+            rn2 = str(cols[4].text.strip()).lower()
+            #rn = re.sub(':', ' ', rn)
 
-        if url == urls[1]:
-            port_data = []
-            table = soup.find('table', id='sort')
-            rows = table.find_all('tr')
+            with open(proxy_list, 'a') as proxy_file:
+                proxy_file.write('%s %s %s\n' % (rn2, rn, rn1))
 
-            for row in rows:                     # Why are xols out of range?
-                cols = row.find_all('td')
-                rn = str(cols[].text.strip())
-                rn1 = str(cols[].text.strip())
-                rn2 = str(cols[].text.strip())
-                type_data.append(rn)
-                port_data.append(rn1)
-                ip_data.append(r2)
-                proxy_file.write('%s %s %s\n' % (rn, rn2, rn1))
+        except IndexError as e:
+            print('%s\nERROR: %s' % (cols, e))
 
+def get_parsed_page_two():
+    url = 'http://www.xroxy.com/free-proxy-lists/?port=&type=Socks5&ssl=&country='
+    sess = requests.Session()
+    req = sess.get(url)
+    content = req.content
+    
+    soup = BeautifulSoup(content, 'lxml')
+    table = soup.find('table', id='DataTables_Table_0')
+    table_body = table.find('tbody')
+    rows = table_body.find_all('tr')
+    
+    for row in rows:
+        cols = row.find_all('td')
+        rn = str(cols[0].text.strip())
+        rn1 = str(cols[1].text.strip())
+        rn2 = str(cols[2].text.strip()).lower()
+        
+        with open(proxy_list, 'a') as proxy_file:
+            proxy_file.write('%s %s %s\n' % (rn2, rn, rn1))
+
+
+def main():
+    if  os.path.exists(proxy_list) == True:
+        os.remove(proxy_list)
+    
     print("Gathering proxy list")
-get_parsed_page()
-print('list writen')
-exit()
+    get_parsed_page_one()
+    get_parsed_page_two()
+
+    print('list complete')
+
+    exit()
+
+if __name__ == '__main__':
+    main()
