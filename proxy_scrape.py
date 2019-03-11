95d13d28e3bbf2731ea703b3a3638bd85c6971f9
hexxend <hexxend@protonmail.com>
2019-03-10 20:05:46 -0400

    Add License to source

--- proxy_scrape.py
+++ proxy_scrape.py
@@ -2,11 +2,32 @@
 """
 scrape ips and proxy type from proxy list sites
 and append them to the proxychains config file
-2018 HexXend
+2018 
 
 TODO;
 scrape more sites
+
+Requires python3.x, request, and BeautifulSoup
+
 """
+#Copyright (C) 2019  HexXend
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#hexxend@protonmail.com
+
 import requests, re 
 from bs4 import BeautifulSoup
 from sys import argv
